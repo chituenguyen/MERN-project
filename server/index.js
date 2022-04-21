50efb748253755e818fa11d4b21aee64bfef3459
chituenguyen <tuenguyen553@gmail.com>
2022-04-21 23:42:59 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./routes/auth");
+require('dotenv').config()
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://tue2910:tue291000@mern.8rglw.mongodb.net/MERNproject?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+connectDB();
+const app = express();
+app.get("/", (req, res) => res.send("Hello man"));
+app.use(express.json())
+app.use('/api/auth',authRouter)
+const PORT = 5000;
+app.listen(PORT, () => console.log("server started"));
